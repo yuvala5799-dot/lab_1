dc3f6910072665440f4d1be572e2c1357ea1d507
yuvala5799-dot <yuvala5799@gmail.com>
2026-03-28 17:54:34 +0300

    Update Shifter.vhd

--- PRE_LAB_DUT/Shifter.vhd
+++ PRE_LAB_DUT/Shifter.vhd
@@ -7,8 +7,8 @@ entity shifter is
 	
 	post (
 		y : IN std_logic_vector(n-1 downto 0 ) ;
-		X_SELECT in std_logic_vector (k-1 downto 0);
-		dir : in std_logic;
+		X_SELECT: in std_logic_vector (k-1 downto 0);
+		alufn : in std_logic_vecyrt(2 downto 0);
 		res : out std_logic_vector(n-1 downto 0 );
 		cout: out std_logic
 		);
@@ -19,14 +19,16 @@ architecture shifter of shifter is
 	subtype vector is sdt_logic_vector (n-1 downto 0);
 	type matrix is array (k-1 downto 0) of vector;
 	signal stages: matrix;
-	
+	signal chack_alufn : std_logic_vector(2 downto 0);
 	signal carries : std_logic_vector(k downto 0);
-	
+
+else res <= (others => '0') ;
+
 begin
+	chack_alufn <= alufn ;
 	stages(0)  <= y;
     carries(0) <= '0';
-	
-	
+
 	shifter_gen: for i in 0 to k-1 generate
         
         stages(i+1) <= stages(i) when x_control(i) = '0'
